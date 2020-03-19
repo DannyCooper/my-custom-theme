7681c3e2e1b202eafd0ab589b1702828571c3e93
Danny Cooper <email@dannycooper.com>
2020-03-19 18:14:48 +0000

    updates

--- functions.php
+++ functions.php
@@ -22,3 +22,11 @@ add_action( 'widgets_init', 'my_custom_theme_sidebar' );
 
 // Add featured image functionality.
 add_theme_support( 'post-thumbnails' );
+
+/**
+ * Enqueue a stylesheet.
+ */
+function my_custom_theme_enqueue() {
+    wp_enqueue_style( 'my-custom-theme', get_stylesheet_uri() );
+}
+add_action( 'wp_enqueue_scripts', 'my_custom_theme_enqueue' );
